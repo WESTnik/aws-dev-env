f6ad235e7c2b0e039e01b328dab27a79c245a00f
Paul <Lisenok.p@gmail.com>
2021-11-18 16:58:04 +0300

    Create: lambda function in IaC

--- main.tf
+++ main.tf
@@ -9,6 +9,38 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region = var.aws_region
+  region                  = var.aws_region
+  shared_credentials_file = var.aws_cred_file
+  profile                 = "new"
 
+}
+
+data "archive_file" "myzip" {
+  type        = "zip"
+  source_file = "main.py"
+  output_path = "main.zip"
+}
+resource "aws_lambda_function" "my_python_lambda" {
+  filename      = "main.zip"
+  function_name = "my_python_lambda_test"
+  role          = aws_iam_role.my_python_lambda_role.arn
+  handler       = "main.lambda_handler"
+  runtime       = "python3.8"
+}
+
+resource "aws_iam_role" "my_python_lambda_role" {
+  name = "my_python_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
--- variables.tf
+++ variables.tf
@@ -2,5 +2,11 @@ variable "aws_region" {
   type        = string
   default     = "us-west-2"
   description = "Default AWS region for use in my free-tier account"
+}
+
+variable "aws_cred_file" {
+  type    = string
+  default = "/c/Users/Pavel_Lysianok/.aws"
+  description = "Default path to AWS credential file"
 
 }
